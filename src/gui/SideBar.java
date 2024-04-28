da2a37f686541063f48d108d1269fb6a5078eb79
understann <nattapon.how@gmail.com>
2024-04-28 22:15:36 +0700

    Your SCore layout still not accurate
    Task list from top to bottom

--- src/gui/SideBar.java
+++ src/gui/SideBar.java
@@ -1,9 +1,13 @@
 package gui;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class SideBar {
     public VBox initializeSidebar(){
@@ -13,18 +17,69 @@ public class SideBar {
         double leftMargin = 40;
 
 
-
+        // SideBar Container
         VBox sideBarDiv = new VBox();
         sideBarDiv.setId("sideBarDiv");
         sideBarDiv.getStylesheets().add(getClass().getResource("/side_bar.css").toExternalForm());
+        sideBarDiv.setPrefWidth(240);
+        sideBarDiv.setAlignment(Pos.TOP_CENTER);
         Insets margins = new Insets(topMargin, rightMargin, bottomMargin, leftMargin);
         VBox.setMargin(sideBarDiv, margins);
 
-        Rectangle rect1 = new Rectangle(250, 50, Color.BLUE);
-        Rectangle rect2 = new Rectangle(250, 50, Color.GREEN);
-        Rectangle rect3 = new Rectangle(250, 50, Color.RED);
+        //BlindBox Container
+        StackPane blindBoxStackPane = new StackPane();
+        blindBoxStackPane.setPadding(new Insets(6,0,0,0));
+        Rectangle blindBox = new Rectangle(225, 60, Color.web("03071C"));
+        blindBox.setArcWidth(15);
+        blindBox.setArcHeight(15);
+        Text blindText = new Text("Blind 1");
+        blindText.getStyleClass().add("blind-text-style");
+        blindBoxStackPane.getChildren().addAll(blindBox,blindText);
 
-        sideBarDiv.getChildren().addAll(rect1,rect2,rect3);
+        //GoalBox Container
+        StackPane goalBoxStackPane = new StackPane();
+        goalBoxStackPane.setPadding(new Insets(10,0,0,0));
+        HBox goalHBox = new HBox(10);
+        goalHBox.setAlignment(Pos.CENTER);
+        goalHBox.setPrefWidth(225);
+        StackPane goalScoreStackPane = new StackPane();
+        Rectangle goalBox = new Rectangle(225, 60, Color.web("03071C"));
+        goalBox.setArcWidth(15);
+        goalBox.setArcHeight(15);
+        Text goalText = new Text("Goal");
+        goalText.getStyleClass().add("goal-text-style");
+        Rectangle goalScoreBox = new Rectangle(110,40, Color.WHITE);
+        goalScoreBox.setArcWidth(10);
+        goalScoreBox.setArcHeight(10);
+        goalScoreBox.getStyleClass().add("goal-score-box-style");
+        Text goalScoreNumberText = new Text("750");
+        goalScoreNumberText.getStyleClass().add("goal-score-number-text-style");
+        goalScoreStackPane.getChildren().addAll(goalScoreBox,goalScoreNumberText);
+        goalHBox.getChildren().addAll(goalText,goalScoreStackPane);
+        goalBoxStackPane.getChildren().addAll(goalBox, goalHBox);
+
+        // Your Score Box - StackPane (Rec, VBOX(Text, StackPane(rectangle, Text)))
+        StackPane yourScoreStackPane = new StackPane();
+        yourScoreStackPane.setPadding(new Insets(10,0,0,0));
+        Rectangle yourScoreBox = new Rectangle(225, 120, Color.web("#595D64"));
+        yourScoreBox.setArcWidth(10);
+        yourScoreBox.setArcHeight(10);
+        VBox yourScoreVBox = new VBox();
+        yourScoreVBox.setPrefHeight(120);
+        Text yourScoreText = new Text("Your score");
+        yourScoreText.getStyleClass().add("your-score-text-style");
+        StackPane yourScoreNumberStackPane = new StackPane();
+        Rectangle yourScoreNumberBox = new Rectangle(200,60, Color.web("#2E333A"));
+        yourScoreNumberBox.setArcWidth(10);
+        yourScoreNumberBox.setArcHeight(10);
+        Text yourScoreNumberText =  new Text("320");
+        yourScoreNumberText.getStyleClass().add("your-score-number-text-style");
+        yourScoreNumberStackPane.getChildren().addAll(yourScoreNumberBox, yourScoreNumberText);
+        yourScoreVBox.getChildren().addAll(yourScoreText,yourScoreNumberStackPane);
+        yourScoreStackPane.getChildren().addAll(yourScoreBox, yourScoreVBox);
+
+
+        sideBarDiv.getChildren().addAll(blindBoxStackPane,goalBoxStackPane, yourScoreStackPane);
 
         return sideBarDiv;
     }
